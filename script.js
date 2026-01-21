6178d91f20faefcc72d8ecfee8132258ae003fe8
velvys0 <67652971+velvys0@users.noreply.github.com>
2026-01-21 15:54:30 +0100

    Make the button update the resolution using flexbox

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const container = document.querySelector("#container");
 
-resolution = 16
+let resolution = 16
 
+function newGrid() {
+    if (document.querySelector('.divSquare') !== null) {
+        allDivRow = document.querySelectorAll(".divRow")
+        allDivRow.forEach((row) => {
+            container.removeChild(row) 
+        })
+    }
 
-for (i = 0; i < resolution; i++) {
+    for (i = 0; i < resolution; i++) {
     let divRow = document.createElement("div");
     divRow.classList.add('divRow')
     container.appendChild(divRow)
@@ -11,21 +18,22 @@ for (i = 0; i < resolution; i++) {
         const divSquare = document.createElement("div");
         divSquare.classList.add('divSquare')
         divRow.appendChild(divSquare)
+        }
     }
+
+    allDivSquare = document.querySelectorAll(".divSquare")
+    allDivSquare.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.style.cssText = 'background-color: red'
+        })
+    })
 }
 
-const allDivSquare = document.querySelectorAll(".divSquare")
-
-allDivSquare.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.style.cssText = 'background-color: red'
-    })
-})
-
+newGrid()
 
 const buttonChangePixels = document.querySelector('button')
-
 buttonChangePixels.addEventListener('click', () => {
     let newRes = prompt("Entrer une résolution de pixels jusqu'à 100")
     resolution = newRes
+    newGrid()
 })
