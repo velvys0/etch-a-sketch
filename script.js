3dd6d18fcb6283c18d0cef35043f450b842b73e7
velvys0 <67652971+velvys0@users.noreply.github.com>
2026-01-21 14:43:47 +0100

    convert the grid to flex

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const container = document.querySelector("#container");
 
+
+
+
 for (i = 0; i < 16; i++) {
     let divRow = document.createElement("div");
     divRow.classList.add('divRow')
     container.appendChild(divRow)
     for (j = 0; j < 16; j++) {
-        const squareDiv3 = document.createElement("div");
-        squareDiv3.classList.add('divSquare')
-        divRow.appendChild(squareDiv3)
+        const divSquare = document.createElement("div");
+        divSquare.classList.add('divSquare')
+        divRow.appendChild(divSquare)
     }
 }
+
+const allDivSquare = document.querySelectorAll(".divSquare")
+
+allDivSquare.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.style.cssText = 'background-color: red'
+    })
+})
+
+const buttonChangePixels = document.querySelector('button')
+
+buttonChangePixels.addEventListener('click', () => console.log('wahou'))
